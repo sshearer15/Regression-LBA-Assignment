d4b737dbefb8a6a9f5c743cd81d9bf9acd449cc9
sshearer15 <33505139+sshearer15@users.noreply.github.com>
2017-11-16 11:19:26 -0800

    Add files via upload

--- Spurious Correlations.R
+++ Spurious Correlations.R
@@ -2,8 +2,9 @@
 
 library("ggplot2")
 
-par(mfrow=c(2,2))
-n <- c(100, 250, 500, 1000)
+# revised replication of professor's in class function
+n <- c(100)
+set.seed(1)
 for (j in n){
   mycor <- rep(0,1000)
   for (i in 1:1000){
@@ -13,4 +14,36 @@ for (j in n){
   }
   hist(mycor, xlim = c(-.4, .4), main = paste('Sample size:', j))
 }
+
+#plotting qqplot
+par(mfrow=c(3,3))
+n <- c(10, 50, 100, 250, 1000, 10000)
+set.seed(1)
+for (j in n){
+  mycor <- rep(0,1000)
+  for (i in 1:1000){
+    x <- rnorm(j)
+    y <- rnorm(j)
+    mycor[i]<- cor(x,y)
+  }
+  qqnorm(mycor, main = paste('Sample size:', j))
+  qqline(mycor, col='red')
+}
+
+#plotting histograms w/ normal distribution curve
+par(mfrow=c(2,3))
+n <- c(10, 50, 100, 250, 1000, 10000)
+set.seed(1)
+for (j in n){
+  mycor <- rep(0,1000)
+  for (i in 1:1000){
+    x <- rnorm(j)
+    y <- rnorm(j)
+    mycor[i] <- cor(x,y)
+  }
+  hist(mycor, probability = TRUE, xlim = c(-1, 1), ylim =c(0,8), main = paste('Sample size:', j))
+  curve(dnorm(x, 0, sd(mycor)), add = TRUE, col = 'red')
+}
+
+
 
